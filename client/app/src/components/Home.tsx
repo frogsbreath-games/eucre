5d31f681cbfff1f1169948ee5170d6e870fb4547
Pierce Lauring <pclauring@gmail.com>
2020-12-14 15:58:35 -0500

    Select input ui component and Home page junk

--- client/app/src/components/Home.tsx
+++ client/app/src/components/Home.tsx
@@ -1,12 +1,65 @@
 import * as React from "react";
 import { connect } from "react-redux";
 import Card from "./Card/Card";
+import { Button, Input, Select } from "../ui";
 
-const Home = () => (
-  <div>
-    <h1>Eucre</h1>
-    <Card front={true} value={7} suit="Hearts" />
-  </div>
-);
+interface HomeState {
+  cardSuit?: string;
+  cardValue?: number;
+  input: string;
+  inputInt: number;
+}
+class Home extends React.Component<{}, HomeState> {
+  componentWillMount() {
+    this.setState({
+      input: "",
+      cardSuit: "Clubs",
+      cardValue: 12,
+      inputInt: 1,
+    });
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSelectChange = this.handleSelectChange.bind(this);
+  }
+
+  handleChange(inputValue: string) {
+    this.setState({ input: inputValue, inputInt: parseInt(inputValue) });
+  }
+
+  handleSelectChange(selectValue: string) {
+    this.setState({ cardSuit: selectValue });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Eucre</h1>
+        <Button>Empty Button</Button>
+        <Input
+          type="number"
+          value={this.state.input ? this.state.input : ""}
+          onChange={this.handleChange}
+          min="1"
+          max="13"
+        />
+        <Select
+          value={this.state.cardSuit ? this.state.cardSuit : "Hearts"}
+          onChange={this.handleSelectChange}
+          options={[
+            { label: "Clubs", value: "Clubs" },
+            { label: "Hearts", value: "Hearts" },
+            { label: "Diamonds", value: "Diamonds" },
+            { label: "Spades", value: "Spades" },
+          ]}
+        />
+        <Card
+          front={true}
+          value={this.state.inputInt}
+          suit={this.state.cardSuit}
+        />
+      </div>
+    );
+  }
+}
 
 export default connect()(Home);
---
+++ client/app/src/ui/Select/Select.tsx
@@ -0,0 +1,37 @@
+import * as React from "react";
+import styles from "./Select.module.css";
+
+interface SelectOption {
+  label: string;
+  value: string;
+}
+interface IProps
+  extends Omit<
+    React.SelectHTMLAttributes<HTMLSelectElement>,
+    "onChange" | "value"
+  > {
+  value: string;
+  onChange(value: string): void;
+  options: Array<SelectOption>;
+}
+
+const Select: React.FunctionComponent<IProps> = ({
+  children,
+  onChange,
+  options,
+  ...shared
+}) => {
+  return (
+    <select
+      className={styles.select}
+      onChange={(e) => onChange(e.target.value)}
+      {...shared}
+    >
+      {options.map((option) => (
+        <option value={option.value}>{option.label}</option>
+      ))}
+    </select>
+  );
+};
+
+export default Select;
