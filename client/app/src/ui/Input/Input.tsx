730c5c235bf86be4dc4d592657a8e63ecfa35215
Pierce Lauring <pclauring@gmail.com>
2020-12-14 14:45:57 -0500

    Input ui component

---
+++ client/app/src/ui/Input/Input.tsx
@@ -0,0 +1,32 @@
+import * as React from "react";
+import styles from "./Input.module.css";
+
+interface IProps
+  extends Omit<
+    React.InputHTMLAttributes<HTMLInputElement>,
+    "onChange" | "value"
+  > {
+  value: string;
+
+  onChange(value: string): void;
+}
+
+const Input: React.FunctionComponent<IProps> = ({
+  children,
+  onChange,
+  ...shared
+}) => {
+  return (
+    <input
+      className={styles.input}
+      onChange={(e) => onChange(e.target.value)}
+      {...shared}
+    />
+  );
+};
+
+export default Input;
+
+Input.defaultProps = {
+  type: "text",
+};
