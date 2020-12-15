32b3087e05a3a642347d59dddb49418758dc4679
Pierce Lauring <pclauring@gmail.com>
2020-12-15 16:40:11 -0500

    Moved Page Components to Appropriate folder

--- client/app/src/App.tsx
+++ client/app/src/App.tsx
@@ -1,16 +1,16 @@
-import * as React from 'react';
-import { Route } from 'react-router';
-import Layout from './components/Layout';
-import Home from './components/Home';
-import Counter from './components/Counter';
-import FetchData from './components/FetchData';
+import * as React from "react";
+import { Route } from "react-router";
+import Layout from "./components/Layout";
+import Home from "./pages/Home/Home";
+import Counter from "./pages/Counter/Counter";
+import FetchData from "./pages/FetchData/FetchData";
 
-import './custom.css'
+import "./custom.css";
 
 export default () => (
   <Layout>
-    <Route exact path='/' component={Home} />
-    <Route path='/counter' component={Counter} />
-    <Route path='/fetch-data/:startDateIndex?' component={FetchData} />
+    <Route exact path="/" component={Home} />
+    <Route path="/counter" component={Counter} />
+    <Route path="/fetch-data/:startDateIndex?" component={FetchData} />
   </Layout>
 );
---
+++ client/app/src/pages/Home/Home.tsx
@@ -0,0 +1,85 @@
+import * as React from "react";
+import { connect } from "react-redux";
+import Card from "../../components/Card/Card";
+import { Button, Input, Select } from "../../ui";
+import styles from "./Home.module.css";
+
+interface HomeState {
+  cardSuit?: string;
+  cardValue?: number;
+  input: string;
+  inputInt: number;
+}
+class Home extends React.Component<{}, HomeState> {
+  componentWillMount() {
+    this.setState({
+      input: "1",
+      cardSuit: "Clubs",
+      cardValue: 12,
+      inputInt: 1,
+    });
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSelectChange = this.handleSelectChange.bind(this);
+    this.handleResetClick = this.handleResetClick.bind(this);
+  }
+
+  handleChange(inputValue: string) {
+    this.setState({ input: inputValue, inputInt: parseInt(inputValue) });
+  }
+
+  handleSelectChange(selectValue: string) {
+    this.setState({ cardSuit: selectValue });
+  }
+
+  handleResetClick() {
+    this.setState({
+      input: "1",
+      cardSuit: "Clubs",
+      cardValue: 12,
+      inputInt: 1,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Eucre</h1>
+        <div>
+          <Button onClick={this.handleResetClick}>Reset</Button>
+          <Input
+            type="number"
+            value={this.state.input ? this.state.input : ""}
+            onChange={this.handleChange}
+            min="1"
+            max="13"
+          />
+          <Select
+            value={this.state.cardSuit ? this.state.cardSuit : "Hearts"}
+            onChange={this.handleSelectChange}
+            options={[
+              { label: "Clubs", value: "Clubs" },
+              { label: "Hearts", value: "Hearts" },
+              { label: "Diamonds", value: "Diamonds" },
+              { label: "Spades", value: "Spades" },
+            ]}
+          />
+        </div>
+        <div className={styles.cardDisplayArea}>
+          <div className={styles.card}>
+            <Card
+              front={true}
+              value={this.state.inputInt}
+              suit={this.state.cardSuit}
+            />
+          </div>
+          <div className={styles.card}>
+            <Card front={false} />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default connect()(Home);
