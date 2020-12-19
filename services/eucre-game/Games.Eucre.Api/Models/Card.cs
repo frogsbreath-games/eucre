3fadd1616a4d71464b1c8cede206573e084b7aa4
Benjamin Chodos <bdchodos@gmail.com>
2020-12-18 22:03:50 -0500

    Fixed warning and introduced additional code style rules

--- services/eucre-game/Games.Eucre.Api/Models/Card.cs
+++ services/eucre-game/Games.Eucre.Api/Models/Card.cs
@@ -1,16 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Games.Eucre.Api.Enums;
-
-namespace Games.Eucre.Api.Models
+﻿namespace Games.Eucre.Api.Models
 {
 	public class Card
 	{
-		public String Suit { get; set; }
+		public string Suit { get; set; } = string.Empty;
 
 		public int Value { get; set; }
-
 	}
 }
