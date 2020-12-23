83130624de16dd549f58c851276aaf62cd48ac10
Pierce Lauring <pclauring@gmail.com>
2020-12-22 23:27:13 -0500

    Table UI component no styles

---
+++ client/app/src/ui/Table/Table.tsx
@@ -0,0 +1,36 @@
+import * as React from "react";
+import { convertCompilerOptionsFromJson } from "typescript";
+import styles from "./Table.module.css";
+
+interface Column {
+  name: string;
+  key: string;
+}
+
+interface IProps {
+  columns: Column[];
+  data: Array<any>;
+}
+
+const Table: React.FC<IProps> = ({ columns, data }) => (
+  <table>
+    <thead>
+      <tr>
+        {columns.map((column) => (
+          <th>{column.name}</th>
+        ))}
+      </tr>
+      {data.map((row) => (
+        <tr>
+          {columns.map((field) => (
+            <td>{row[field.key]}</td>
+          ))}
+        </tr>
+      ))}
+    </thead>
+  </table>
+);
+
+export default Table;
+
+Table.defaultProps = {};
