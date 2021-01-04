b725138ab8fb9638540e2a3338f3c96eb06307bf
Pierce Lauring <pclauring@gmail.com>
2021-01-04 14:55:24 -0500

    Adding test dnd context for card test

--- client/app/src/games/eucre/components/Card/Card.test.tsx
+++ client/app/src/games/eucre/components/Card/Card.test.tsx
@@ -1,14 +1,18 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import Card from "./Card";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 
 it("renders without crashing", () => {
-  const cardProps = ({
-    value: 5
-  });
+  const cardProps = {
+    value: 5,
+  };
 
   ReactDOM.render(
-    <Card {...cardProps} />,
+    <DndProvider backend={HTML5Backend}>
+      <Card {...cardProps} />
+    </DndProvider>,
     document.createElement("div")
   );
 });
